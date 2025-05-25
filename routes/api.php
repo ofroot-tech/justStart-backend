3eb1ca7bb70e8c9d531afd305b4c7528782fc4ea
dimitri mcdaniel <dimitri@circleclick.com>
2025-05-25 14:33:19 -0500

    updating

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,11 @@
 <?php
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/sign-in', [UserController::class, 'signIn']);
+Route::post('/forgot-password', [UserController::class, 'forgotPassword']);
+Route::post('/update-password', [UserController::class, 'updatePassword']);
