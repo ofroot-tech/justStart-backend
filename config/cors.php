98fb38b222aa32e0317248a73426df13ecac9742
dimitri mcdaniel <dimitri@circleclick.com>
2025-05-26 07:57:48 -0500

    push

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://your-frontend-domain.com'], // Replace with your frontend domain
+    'allowed_origins' => ['https://ofroot.technology'], // Replace with your frontend domain
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
